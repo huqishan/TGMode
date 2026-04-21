0fa9d21dc73917b4df5dfe8f29686aff5f7fc441
Administrator <347162668@qq.com>
2026-04-21 18:53:47 +0800

    1

---
+++ Shared/Models/Log/LogMessageModel.cs
@@ -0,0 +1,20 @@
+﻿using Shared.Abstractions.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models.Log
+{
+    public class LogMessageModel
+    {
+        public DateTime LogTime { get; set; } = DateTime.Now;
+        public string Message { get; set; }
+        public LogType Type { get; set; }
+        public override string ToString()
+        {
+            return $"时间：{LogTime} 消息类型：{Type}\r\n 内容：{Message}";
+        }
+    }
+}
